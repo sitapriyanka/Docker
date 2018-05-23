db873e60a85229906ec28846a18613cc9fefb147
Prasanth Balam <prasanthbalam@gmail.com>
2018-05-23 22:29:27 +0530

    MyFirstDockerApp

---
+++ HellofromCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HellofromCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
